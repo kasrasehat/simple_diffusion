8b1a30c2c0462b0cbd6560be99e6e443c4b3e858
kasra sehat <kasra.sehat@yahoo.com>
2023-11-29 09:49:19 +0330

    complete controllable diff

---
+++ test_controllable_dif.py
@@ -0,0 +1,125 @@
+from typing import Dict, Tuple
+from tqdm import tqdm
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+from torchvision import models, transforms
+from torchvision.utils import save_image, make_grid
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation, PillowWriter
+import numpy as np
+from IPython.display import HTML
+from diffusion_utilities import *
+from networks import ContextUnet
+
+# hyperparameters
+
+# diffusion hyperparameters
+timesteps = 500
+beta1 = 1e-4
+beta2 = 0.02
+
+# network hyperparameters
+device = torch.device("cuda:0" if torch.cuda.is_available() else torch.device('cpu'))
+n_feat = 64 # 64 hidden dimension feature
+n_cfeat = 5 # context vector is of size 5
+height = 16 # 16x16 image
+save_dir = './weights/'
+
+# training hyperparameters
+batch_size = 100
+n_epoch = 32
+lrate=1e-3
+
+# construct DDPM noise schedule
+b_t = (beta2 - beta1) * torch.linspace(0, 1, timesteps + 1, device=device) + beta1
+a_t = 1 - b_t
+ab_t = torch.cumsum(a_t.log(), dim=0).exp()
+ab_t[0] = 1
+
+# construct model
+nn_model = ContextUnet(in_channels=3, n_feat=n_feat, n_cfeat=n_cfeat, height=height).to(device)
+# load dataset and construct optimizer
+dataset = CustomDataset("./sprites_1788_16x16.npy", "./sprite_labels_nc_1788_16x16.npy", transform, null_context=False)
+dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=1)
+optim = torch.optim.Adam(nn_model.parameters(), lr=lrate)
+
+# load in pretrain model weights and set to eval mode
+nn_model.load_state_dict(torch.load(f"{save_dir}/context_model_trained.pth", map_location=device))
+nn_model.eval()
+print("Loaded in Context Model")
+
+
+def perturb_input(x, t, noise):
+    return ab_t.sqrt()[t, None, None, None] * x + (1 - ab_t[t, None, None, None]) * noise
+
+# helper function; removes the predicted noise (but adds some noise back in to avoid collapse)
+
+
+def denoise_add_noise(x, t, pred_noise, z=None):
+    if z is None:
+        z = torch.randn_like(x)
+    noise = b_t.sqrt()[t] * z
+    mean = (x - pred_noise * ((1 - a_t[t]) / (1 - ab_t[t]).sqrt())) / a_t[t].sqrt()
+    return mean + noise
+
+
+# sample with context using standard algorithm
+@torch.no_grad()
+def sample_ddpm_context(n_sample, context, save_rate=20):
+    # x_T ~ N(0, 1), sample initial noise
+    samples = torch.randn(n_sample, 3, height, height).to(device)
+
+    # array to keep track of generated steps for plotting
+    intermediate = []
+    for i in range(timesteps, 0, -1):
+        print(f'sampling timestep {i:3d}', end='\r')
+
+        # reshape time tensor
+        t = torch.tensor([i / timesteps])[:, None, None, None].to(device)
+
+        # sample some random noise to inject back in. For i = 1, don't add back in noise
+        z = torch.randn_like(samples) if i > 1 else 0
+
+        eps = nn_model(samples, t, c=context)    # predict noise e_(x_t,t, ctx)
+        samples = denoise_add_noise(samples, i, eps, z)
+        if i % save_rate==0 or i==timesteps or i<8:
+            intermediate.append(samples.detach().cpu().numpy())
+
+    intermediate = np.stack(intermediate)
+    return samples, intermediate
+
+# visualize samples with randomly selected context
+plt.clf()
+ctx = F.one_hot(torch.randint(0, 5, (32,)), 5).to(device=device).float()
+samples, intermediate = sample_ddpm_context(32, ctx)
+animation_ddpm_context = plot_sample(intermediate,32,4,save_dir, "ani_run", None, save=False)
+HTML(animation_ddpm_context.to_jshtml())
+
+
+def show_images(imgs, nrow=2):
+    _, axs = plt.subplots(nrow, imgs.shape[0] // nrow, figsize=(4,2 ))
+    axs = axs.flatten()
+    for img, ax in zip(imgs, axs):
+        img = (img.permute(1, 2, 0).clip(-1, 1).detach().cpu().numpy() + 1) / 2
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ax.imshow(img)
+    plt.show()
+
+
+# user defined context
+ctx = torch.tensor([
+    # hero, non-hero, food, spell, side-facing
+    [1,0,0,0,0],
+    [1,0,0,0,0],
+    [0,0,0,0,1],
+    [0,0,0,0,1],
+    [0,1,0,0,0],
+    [0,1,0,0,0],
+    [0,0,1,0,0],
+    [0,0,1,0,0],
+]).float().to(device)
+samples, _ = sample_ddpm_context(ctx.shape[0], ctx)
+show_images(samples)
--- train_controllable_dif.py
+++ train_controllable_dif.py
@@ -61,9 +61,14 @@ for ep in range(n_epoch):
     optim.param_groups[0]['lr'] = lrate * (1 - ep / n_epoch)
 
     pbar = tqdm(dataloader, mininterval=2)
-    for x, _ in pbar:  # x: images
+    for x, c in pbar:  # x: images  c: context
         optim.zero_grad()
         x = x.to(device)
+        c = c.to(x)
+
+        # randomly mask out c
+        context_mask = torch.bernoulli(torch.zeros(c.shape[0]) + 0.9).to(device)
+        c = c * context_mask.unsqueeze(-1)
 
         # perturb data
         noise = torch.randn_like(x)
@@ -71,7 +76,7 @@ for ep in range(n_epoch):
         x_pert = perturb_input(x, t, noise)
 
         # use network to recover noise
-        pred_noise = nn_model(x_pert, t / timesteps)
+        pred_noise = nn_model(x_pert, t / timesteps, c=c)
 
         # loss is mean squared error between the predicted and true noise
         loss = F.mse_loss(pred_noise, noise)
@@ -83,5 +88,5 @@ for ep in range(n_epoch):
     if ep % 4 == 0 or ep == int(n_epoch - 1):
         if not os.path.exists(save_dir):
             os.mkdir(save_dir)
-        torch.save(nn_model.state_dict(), save_dir + f"model_{ep}.pth")
-        print('saved model at ' + save_dir + f"model_{ep}.pth")
+        torch.save(nn_model.state_dict(), save_dir + f"context_model_{ep}.pth")
+        print('saved model at ' + save_dir + f"context_model_{ep}.pth")
