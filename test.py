769a994153c942cdb438d6886fc3c9892872a07b
kasra sehat <kasra.sehat@yahoo.com>
2023-11-29 09:29:26 +0330

    create train, test and controllable diffusion models

---
+++ test.py
@@ -0,0 +1,112 @@
+from typing import Dict, Tuple
+from tqdm import tqdm
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+from torchvision import models, transforms
+from torchvision.utils import save_image, make_grid
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation, PillowWriter
+import numpy as np
+from IPython.display import HTML
+from diffusion_utilities import *
+from networks import ContextUnet
+
+# hyperparameters
+
+# diffusion hyperparameters
+timesteps = 500
+beta1 = 1e-4
+beta2 = 0.02
+
+# network hyperparameters
+device = torch.device("cuda:0" if torch.cuda.is_available() else torch.device('cpu'))
+n_feat = 64 # 64 hidden dimension feature
+n_cfeat = 5 # context vector is of size 5
+height = 16 # 16x16 image
+save_dir = './weights/'
+
+# construct DDPM noise schedule
+b_t = (beta2 - beta1) * torch.linspace(0, 1, timesteps + 1, device=device) + beta1
+a_t = 1 - b_t
+ab_t = torch.cumsum(a_t.log(), dim=0).exp()
+ab_t[0] = 1
+# construct model
+nn_model = ContextUnet(in_channels=3, n_feat=n_feat, n_cfeat=n_cfeat, height=height).to(device)
+# helper function; removes the predicted noise (but adds some noise back in to avoid collapse)
+
+
+def denoise_add_noise(x, t, pred_noise, z=None):
+    if z is None:
+        z = torch.randn_like(x)
+    noise = b_t.sqrt()[t] * z
+    mean = (x - pred_noise * ((1 - a_t[t]) / (1 - ab_t[t]).sqrt())) / a_t[t].sqrt()
+    return mean + noise
+
+# load in model weights and set to eval mode
+nn_model.load_state_dict(torch.load(f"{save_dir}/model_trained.pth", map_location=device))
+nn_model.eval()
+print("Loaded in Model")
+# sample using standard algorithm
+@torch.no_grad()
+def sample_ddpm(n_sample, save_rate=20):
+    # x_T ~ N(0, 1), sample initial noise
+    samples = torch.randn(n_sample, 3, height, height).to(device)
+
+    # array to keep track of generated steps for plotting
+    intermediate = []
+    for i in range(timesteps, 0, -1):
+        print(f'sampling timestep {i:3d}', end='\r')
+
+        # reshape time tensor
+        t = torch.tensor([i / timesteps])[:, None, None, None].to(device)
+
+        # sample some random noise to inject back in. For i = 1, don't add back in noise
+        z = torch.randn_like(samples) if i > 1 else 0
+
+        eps = nn_model(samples, t)    # predict noise e_(x_t,t)
+        samples = denoise_add_noise(samples, i, eps, z)
+        if i % save_rate == 0 or i == timesteps or i < 8:
+            intermediate.append(samples.detach().cpu().numpy())
+
+    intermediate = np.stack(intermediate)
+    return samples, intermediate
+
+# visualize samples
+plt.clf()
+samples, intermediate_ddpm = sample_ddpm(32)
+animation_ddpm = plot_sample(intermediate_ddpm,32,4,save_dir, "ani_run", None, save=False)
+HTML(animation_ddpm.to_jshtml())
+
+# incorrectly sample without adding in noise
+@torch.no_grad()
+def sample_ddpm_incorrect(n_sample):
+    # x_T ~ N(0, 1), sample initial noise
+    samples = torch.randn(n_sample, 3, height, height).to(device)
+
+    # array to keep track of generated steps for plotting
+    intermediate = []
+    for i in range(timesteps, 0, -1):
+        print(f'sampling timestep {i:3d}', end='\r')
+
+        # reshape time tensor
+        t = torch.tensor([i / timesteps])[:, None, None, None].to(device)
+
+        # don't add back in noise
+        z = 0
+
+        eps = nn_model(samples, t)    # predict noise e_(x_t,t)
+        samples = denoise_add_noise(samples, i, eps, z)
+        if i%20==0 or i==timesteps or i<8:
+            intermediate.append(samples.detach().cpu().numpy())
+
+    intermediate = np.stack(intermediate)
+    return samples, intermediate
+
+# visualize samples
+plt.clf()
+samples, intermediate = sample_ddpm_incorrect(32)
+animation = plot_sample(intermediate,32, 4, save_dir, "ani_run", None, save=False)
+HTML(animation.to_jshtml())
+
